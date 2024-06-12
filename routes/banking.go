0a5190d403b64304d6c146f99e76365baa3d2695
tahmid-saj <tahmid.saj@gmail.com>
2024-06-12 03:06:19 -0400

    banking crud

--- routes/banking.go
+++ routes/banking.go
@@ -7,7 +7,7 @@ import (
 )
 
 // banking accounts
-func getBankingAccounts(context *gin.Context) {
+func getAllBankingAccounts(context *gin.Context) {
 	bankingAccounts, err := models.GetAllBankingAccounts()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch banking accounts"})
@@ -17,12 +17,35 @@ func getBankingAccounts(context *gin.Context) {
 	context.JSON(http.StatusOK, bankingAccounts)
 }
 
-func getBankingAccount(context *gin.Context) {
+func getBankingAccountsByUser(context *gin.Context) {
+	userId := context.Param("userid")
+	email := context.Param("email")
 
+	bankingAccounts, err := models.GetBankingAccountsByUser(userId, email)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch banking accounts"})
+		return
+	}
+
+	context.JSON(http.StatusOK, bankingAccounts)
 }
 
 func createBankingAccount(context *gin.Context) {
+	var bankingAccount models.BankingAccount
+	err := context.ShouldBindJSON(&bankingAccount)
 
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = bankingAccount.SaveBankingAccount()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create banking account"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Banking account created", "bankingAccount": bankingAccount})
 }
 
 func updateBankingAccount(context *gin.Context) {
